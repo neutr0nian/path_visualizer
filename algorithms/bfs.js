127d2356d84ef7f85836237b18ede45e79405dae
Pratik Chavan <pratikc@youronlinemarketplace.com>
2023-03-18 17:44:37 -0400

    added path backtracing

--- algorithms/bfs.js
+++ algorithms/bfs.js
@@ -3,10 +3,11 @@ import { blocks } from "../index.js";
 import { getCellNumber } from "../utils.js";
 
 export default class Bfs {
-  path = new Set();
+  path = {};
   visited = new Set();
   constructor(source, destination) {
     this.source = source;
+    this.sourceCell = getCellNumber(source[0], source[1]);
     this.destCell = getCellNumber(destination[0], destination[1]);
   }
 
@@ -17,10 +18,9 @@ export default class Bfs {
     this.visited.add(0);
     while (q.length != 0) {
       let [row, col] = [...q.shift()];
-      let cellNumber = getCellNumber(row, col);
-      this.path.add(cellNumber);
+      let currentCell = getCellNumber(row, col);
 
-      if (cellNumber === this.destCell) {
+      if (currentCell === this.destCell) {
         return this.tracePath();
       }
 
@@ -28,11 +28,13 @@ export default class Bfs {
       if (row - 1 >= 0 && !this.visited.has(cell) && map[row - 1][col] == 1) {
         this.visited.add(cell);
         q.push([row - 1, col]);
+        this.path[cell] = currentCell;
       }
       cell = getCellNumber(row, col - 1);
       if (col - 1 >= 0 && !this.visited.has(cell) && map[row][col - 1] == 1) {
         this.visited.add(cell);
         q.push([row, col - 1]);
+        this.path[cell] = currentCell;
       }
 
       cell = getCellNumber(row + 1, col);
@@ -43,6 +45,7 @@ export default class Bfs {
       ) {
         this.visited.add(cell);
         q.push([row + 1, col]);
+        this.path[cell] = currentCell;
       }
 
       cell = getCellNumber(row, col + 1);
@@ -53,6 +56,7 @@ export default class Bfs {
       ) {
         this.visited.add(cell);
         q.push([row, col + 1]);
+        this.path[cell] = currentCell;
       }
     }
   }
@@ -65,14 +69,21 @@ export default class Bfs {
         block.block.draw("gray");
       }, 1000 + i);
       i += 100;
-      if (this.path.has(v)) {
-        let color = v === this.destCell ? "green" : "orange";
-        setTimeout(() => {
-          block.block.draw(color);
-        }, 1000 + i + 100);
-      }
-      i += 100;
     });
+
+    let target = this.destCell;
+    const path = [target];
+    while (target != this.sourceCell) {
+      path.push(this.path[target]);
+      target = this.path[target];
+    }
+    for (let p = path.length - 1; p >= 0; p--) {
+      let b = blocks[path[p]];
+      setTimeout(() => {
+        b.block.draw("orange");
+      }, 1000 + i);
+      i += 200;
+    }
   }
 
   findPath() {}
